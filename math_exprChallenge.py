bf5816e9b314dff36e98054650e9d47b70e88338
ShahadAliAldawsari <129079179+ShahadAliAldawsari@users.noreply.github.com>
2024-01-20 13:42:18 +0300

    Update math_exprChallenge.py
    add challenge description image

--- math_exprChallenge.py
+++ math_exprChallenge.py
@@ -1,3 +1,4 @@
+![image](https://github.com/ShahadAliAldawsari/CoderHubChallenges_MySolutions/assets/129079179/bcb7606c-a5b1-462d-97b0-12555e559efc)
 from typing import List
 import re
 def math_expr(expr: str) -> bool:
